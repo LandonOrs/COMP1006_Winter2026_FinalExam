f17a921635de36e70761195a40c5bdbac5d605e3
LandonOrs <fr00gy001@gmail.com>
2026-04-15 15:05:40 -0400

    added dellete function using pdo repair

---
+++ delete.php
@@ -0,0 +1,18 @@
+<?php
+// connecting to database
+require 'db.php';
+
+//get the team id from the query string
+$id = $_GET['id'];
+
+if (isset($id)) {
+    // prepare and execute the delete statement
+    $stmt = $pdo->prepare("DELETE FROM images WHERE id = :id");
+    $stmt = $pdo->prepare("DELETE FROM images WHERE id = :id");
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+}
+
+// redirect back to the index page
+header("Location: index.php");
+exit();
